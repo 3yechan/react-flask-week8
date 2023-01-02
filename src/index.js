991398d52dec33a1e2c97ef362a9d9f9f9b233a9
ojisamacharles <clee0124@gmail.com>
2023-01-02 02:22:35 -0800

    second commit

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App' 
 import { BrowserRouter } from 'react-router-dom'
+import { initializeApp } from "firebase/app";
 const root = ReactDOM.createRoot(document.getElementById('root'))
 root.render(
     <BrowserRouter>
@@ -10,3 +11,20 @@ root.render(
 
 )
 
+// Import the functions you need from the SDKs you need
+
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCBFGXR7Reg1Q46gDxcV8bdlUhCKQxgdnc",
+  authDomain: "fullstackweek8.firebaseapp.com",
+  projectId: "fullstackweek8",
+  storageBucket: "fullstackweek8.appspot.com",
+  messagingSenderId: "508208122965",
+  appId: "1:508208122965:web:e3863fe76e03f2607998dc"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
